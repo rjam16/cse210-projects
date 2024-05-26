58585b8a99303603e28062c0f6c889a0051779ff
rjam16 <jam23019@byui.edu>
2024-05-25 20:30:49 -0700

    reference fix?

--- prove/Develop03/Reference.cs
+++ prove/Develop03/Reference.cs
@@ -1,8 +1,8 @@
 class Reference {
-    private string _book;
-    private int _chapter;
-    private int _verse;
-    private int _endVerse;
+    private static string _book;
+    private static int _chapter;
+    private static int _verse;
+    private static int _endVerse;
     public void OneVerse(string book, int chapter, int verse)
     {
         _book = book;
@@ -16,7 +16,7 @@ class Reference {
         _verse = verse;
         _endVerse = endVerse;
     }
-    public string GetDisplayText()
+    public static string GetDisplayText()
     {
         if (_endVerse > 0)
         {
